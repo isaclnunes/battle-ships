adc9f50455c78c15021534f8fefceab40c090a92
Isac <isacps2@gmail.com>
2015-11-30 00:45:08 +0000

    Starting to code the place ship method

--- src/main/java/battleShips/actions/ShipsPlacement.java
+++ src/main/java/battleShips/actions/ShipsPlacement.java
@@ -7,8 +7,26 @@ import battleShips.models.enums.BattleShips;
 
 public class ShipsPlacement {
 	
-	public void placeShip(Player player, BattleShips ship, Point point){
-		
+	/**
+	 * Place a ship on the player board validating
+	 * if it is been placed horizontal and if the 
+	 * ship is placed in the limits.
+	 * 
+	 * @param player - the player that's placing the ship
+	 * @param ship - the BattleShips to be placed
+	 * @param start - the start {@link Point}
+	 * @param end - the end {@link Point}
+	 */
+	public void placeShip(Player player, BattleShips ship, Point start, Point end){
+		if(isValidPlacement(start, end)) {
+			
+		}
 	}
 
+	private boolean isValidPlacement(Point start, Point end) {
+		if(start.getX() == end.getX() || start.getY() == end.getY()){
+			return true;
+		}
+		return false;
+	}
 }
